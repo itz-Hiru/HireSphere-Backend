737f92816138645fd1eea88c599ac51290c97d26
Nikesh-koila <kirthanbirwaz43@gmail.com>
2025-08-24 10:38:50 +0530

    Feat: Add getUser controller function

--- controllers/authController.controller.js
+++ controllers/authController.controller.js
@@ -76,6 +76,13 @@ export const login = async (req, res) => {
 // @access private
 export const getUser = async (req, res) => {
   try {
+    const user = await User.findById(req.user.id).select("-passowrd");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
