1c97dfee2c3c3184c465046ea1d5012dedb28db9
itz-Hiru <hirumithagithub@gmail.com>
2025-08-24 09:33:53 +0530

    Created uploadImage route

--- routes/authRoutes.route.js
+++ routes/authRoutes.route.js
@@ -5,11 +5,22 @@ import {
   getUser,
 } from "../controllers/authController.controller.js";
 import { protect } from "../middlewares/authMiddleware.middleware.js";
+import { upload } from "../middlewares/uploadMiddleware.middleware.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/get", protect, getUser);
+
+router.post("/upload-image", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "No file choosen" });
+  }
+  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+    req.file.filename
+  }`;
+  res.status(200).json({ imageUrl });
+});
 
 export default router;
