1fe08f845c24f8661e5ce5f4bebb1809cf3d3765
itz-Hiru <hirumithagithub@gmail.com>
2025-08-30 10:16:38 +0530

    Created updateProfile

---
+++ controllers/userController.controller.js
@@ -0,0 +1,39 @@
+import fs from "fs";
+import path from "path";
+import User from "../models/User.model.js";
+
+// @description Update user profile
+// @route /api/user/update/profile
+// @access private
+export const updateProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    if (!user) return res.status(400).json({ message: "User not found" });
+
+    user.name = name || user.name;
+    user.avatar = avatar || user.avatar;
+    user.resume = resume || user.resume;
+
+    // If employer allow updating company info
+    if (user.role === "employer") {
+      user.companyName = companyName || user.companyName;
+      user.companyDescription = companyDescription || user.companyDescription;
+      user.companyLogo = companyLogo || user.companyLogo;
+    }
+
+    await user.save();
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      avatar: user.avatar,
+      role: user.role,
+      companyName: user.companyName,
+      companyDescription: user.companyDescription,
+      companyLogo: user.companyLogo,
+      resume: user.resume || "",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
