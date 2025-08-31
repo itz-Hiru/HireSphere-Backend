93b8492a4788273d89366fac410524d7b84392fc
itz-Hiru <hirumithagithub@gmail.com>
2025-08-31 11:03:19 +0530

    Created get jobs

--- routes/jobRoutes.route.js
+++ routes/jobRoutes.route.js
@@ -13,7 +13,7 @@ import { protect } from "../middlewares/authMiddleware.middleware.js";
 const router = express.Router();
 
 router.post("/create", protect, createJob);
-router.get("/", protect, getJobs);
+router.get("/", getJobs);
 router.get("/get-jobs-employer", protect, getJobsEmployer);
 router.get("/:id", protect, getJobById);
 router.put("/toggle/:id", toggleCloseJob);
